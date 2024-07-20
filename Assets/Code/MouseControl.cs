41a9611ef99958eb7949004875931024c2d67964
Staton Otto <98541089+GammaVolantis@users.noreply.github.com>
2024-07-19 23:28:25 -0400

    Added Mouse movement functionality
    Mouse moves where you want it to.

--- Assets/Code/MouseControl.cs
+++ Assets/Code/MouseControl.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
+    [SerializeField] Camera mouseCam;
+    public float distance;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+           
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //Vector3 mousePos = mouseCam.ScreenToWorldPoint(Input.mousePosition);
+        //mousePos.z = 0f;
+        //transform.position = mousePos;
+        //Debug.Log(mousePos);
+
+        Ray mousePos = mouseCam.ScreenPointToRay(Input.mousePosition);
+        Vector3 visMousePos = mousePos.GetPoint(distance);
+        visMousePos.z = 0;
+        transform.position = visMousePos;
+        Debug.Log(mousePos);
     }
 }
