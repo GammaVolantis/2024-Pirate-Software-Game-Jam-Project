051974b98a1dea5c148ca37d5ea89ea57c6296ca
Staton Otto <98541089+GammaVolantis@users.noreply.github.com>
2024-07-25 21:48:03 -0500

    Revert "PlayerStats and Health Bar Closer to completion"
    This reverts commit 39006616b91d062a6b595f5373bb8360028e459f.

--- Assets/PlayerTest.cs
+++ Assets/PlayerTest.cs
@@ -8,46 +8,33 @@ public class PlayerTest : MonoBehaviour
 {
 
     public PlayerStats player;
-    public GameObject healthFill;
 
     private float healthScaleFactor;
 
     // Start is called before the first frame update
     void Start()
     {
-        healthScaleFactor = healthFill.transform.localScale.x/player.maxHealth;
-        healthFill.transform.localScale = new Vector3(healthScaleFactor * player.curHealth, healthFill.transform.localScale.y,0f);
-        float difHealth = player.curHealth - player.maxHealth;
-        healthFill.transform.position = new Vector3(healthFill.transform.position.x+difHealth*healthScaleFactor/2,healthFill.transform.position.y,0f);
+        Transform playerTransform = gameObject.transform;
+        Transform childTransform = playerTransform.Find("HealthBar/Fill");
+        if (childTransform != null) 
+        {
+            healthScaleFactor = childTransform.localScale.x;
+        }
+        else 
+        { 
+            Debug.Log("Could Not Find Fill!!!"); 
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && player.curHealth>0) 
+        if (Input.GetKeyDown(KeyCode.Space)) 
         {
             //change in health
             player.newHealthValue(player.GetCurrentHealth(),-20);
             //set new player healthbar value
-            healthFill.transform.localScale = new Vector3(healthScaleFactor * player.curHealth, healthFill.transform.localScale.y, 0f);
-            float difHealth = -20;
-            healthFill.transform.position = new Vector3(healthFill.transform.position.x + difHealth * healthScaleFactor / 2, healthFill.transform.position.y, 0f);
-
+            
         }
-        if (Input.GetKeyDown(KeyCode.LeftShift) && player.curHealth <100)
-        {
-            //change in health
-            player.newHealthValue(player.GetCurrentHealth(), 20);
-            //set new player healthbar value
-            healthFill.transform.localScale = new Vector3(healthScaleFactor * player.curHealth, healthFill.transform.localScale.y, 0f);
-            float difHealth = 20;
-            healthFill.transform.position = new Vector3(healthFill.transform.position.x + difHealth * healthScaleFactor / 2, healthFill.transform.position.y, 0f);
-
-        }
-    }
-
-    private void HealthBarUpdate()
-    {
-       
     }
 }
