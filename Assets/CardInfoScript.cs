513e89980002f328bc549a6b687c7804a5839b2d
Staton Otto <98541089+GammaVolantis@users.noreply.github.com>
2024-07-27 04:58:06 -0500

    Finished Visualizing the Card Data

--- Assets/CardInfoScript.cs
+++ Assets/CardInfoScript.cs
@@ -1,18 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CardInfoScript : MonoBehaviour
 {
+    public CardStats card;
+
+    private TextMeshProUGUI nameText;
+    private TextMeshProUGUI target;
+    private TextMeshProUGUI range;
+    private TextMeshProUGUI actionValue;
+    private Image cardArt;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        cardArt = transform.Find("CardArt").GetComponent<Image>();
+        nameText = transform.Find("Name").GetComponent<TextMeshProUGUI>();
+        target = transform.Find("Target").GetComponent<TextMeshProUGUI>();
+        range = transform.Find("Range").GetComponent <TextMeshProUGUI>();
+        actionValue = transform.Find("ActionValue").GetComponent<TextMeshProUGUI>();
+
+        cardArt.sprite = card.artwork;
+        nameText.SetText(card.cardName);
+        target.SetText(card.target);
+        range.SetText(card.range.ToString());
+        actionValue.SetText(card.actionValue.ToString());
+        actionValue.color = card.actionColor;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
